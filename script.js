5e046fd10c317c25bf0e3383464e251eeac5b2fb
orbitals-git <148598964+orbitals-git@users.noreply.github.com>
2023-10-29 00:16:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-var audio = document.getElementById('audio');
-var playPause = document.getElementById('play-pause');
-var progressContainer = document.getElementById('progress-container');
-var progress = document.getElementById('progress');
+// Get the audio element
+const audio = document.getElementById("audio");
 
-playPause.addEventListener('click', function() {
-    if (audio.paused) {
-        audio.play();
-        playPause.style.background = '#ED4956';
-    } else {
-        audio.pause();
-        playPause.style.background = '#1DB954';
-    }
+// Get the play and pause buttons
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+
+// Add event listeners to the buttons
+play.addEventListener("click", function() {
+   // Play the audio
+   audio.play();
 });
 
-audio.addEventListener('timeupdate', function() {
-    var progressPercent = (audio.currentTime / audio.duration) * 100;
-    progress.style.width = progressPercent + '%';
-});
-
-progressContainer.addEventListener('click', function(e) {
-    var clickPosition = e.pageX - progressContainer.offsetLeft;
-    var progressWidth = window.getComputedStyle(progressContainer).getPropertyValue('width');
-    var clickPercent = clickPosition / parseInt(progressWidth) * 100;
-
-    audio.currentTime = audio.duration * (clickPercent / 100);
+pause.addEventListener("click", function() {
+   // Pause the audio
+   audio.pause();
 });
